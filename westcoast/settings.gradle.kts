74aa4fcf76c488b59ea06d319be162283d2e4257
Felipe <spacetug@gmail.com>
2023-01-22 15:01:11 -0800

    Using kts script for build.gradle

--- westcoast/settings.gradle.kts
+++ westcoast/settings.gradle.kts
@@ -1,6 +1,5 @@
 pluginManagement {
     plugins {
-        id("com.github.johnrengelman.shadow") version "7.1.2"
         id("edu.wpi.first.GradleRIO") version "2023.2.1"
         kotlin("jvm") version "1.8.0"
     }
@@ -15,12 +14,14 @@ pluginManagement {
     }
 }
 
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
         maven("https://frcmaven.wpi.edu/artifactory/release/")
+        maven("https://dev.studica.com/maven/release/2023/")
+        maven("https://maven.ctr-electronics.com/release/")
+        maven("https://maven.revrobotics.com/")
     }
 }
